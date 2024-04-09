48c1c6f4d661b8fb791e60b6a8e3b942967b3f1b
Gabriel Firea <144819970+yonutfig@users.noreply.github.com>
2024-04-09 16:15:15 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp";
+import "animate.css/animate.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
